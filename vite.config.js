b21ca247589e422c16ac2c8bc565d53b8c3391be
Prashant Kumar <105093586+prashantkumar342@users.noreply.github.com>
2024-12-06 16:23:35 +0530

    vite.config.js fixed

--- vite.config.js
+++ vite.config.js
@@ -1,54 +1,63 @@
+/* eslint-disable no-undef */
 // vite.config.js
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: [
-        "index-DUa9jhAc.js",
-        "index-DUfX2OhJ.css",
-        "_redirects",
-        "index.html",
-        "manifest.webmanifest",
-        "registerSW.js",
-        "sw.js",
-        "workbox-42774e1b.js",
-      ],
-      manifest: {
-        name: "chatting with fun",
-        short_name: "c4chats",
-        description: "A React app with PWA support",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#ffffff",
-        icons: [],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: new RegExp(
-              `^${import.meta.env.VITE_API_BASE_URL}/api/v1/.*`
-            ),
-            handler: "NetworkFirst",
-            options: {
-              cacheName: "api-cache",
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24, // 1 day
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
+export default ({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: [
+          "index-DUa9jhAc.js",
+          "index-DUfX2OhJ.css",
+          "_redirects",
+          "index.html",
+          "manifest.webmanifest",
+          "registerSW.js",
+          "sw.js",
+          "workbox-42774e1b.js",
+        ],
+        manifest: {
+          name: "chatting with fun",
+          short_name: "c4chats",
+          description: "A React app with PWA support",
+          start_url: "/",
+          display: "standalone",
+          background_color: "#ffffff",
+          theme_color: "#ffffff",
+          icons: [],
+        },
+        workbox: {
+          runtimeCaching: [
+            {
+              urlPattern: new RegExp(`^${env.VITE_API_BASE_URL}/api/v1/.*`),
+              handler: "NetworkFirst",
+              options: {
+                cacheName: "api-cache",
+                expiration: {
+                  maxEntries: 50,
+                  maxAgeSeconds: 60 * 60 * 24, // 1 day
+                },
+                cacheableResponse: {
+                  statuses: [0, 500],
+                },
               },
             },
-          },
-        ],
-      },
-    }),
-  ],
-});
+          ],
+        },
+      }),
+    ],
+    define: {
+      "import.meta.env.VITE_API_BASE_URL": JSON.stringify(
+        env.VITE_API_BASE_URL
+      ),
+    },
+  });
+};
